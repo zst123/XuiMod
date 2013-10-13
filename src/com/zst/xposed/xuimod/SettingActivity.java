da16176440eb780ec64c6c72962654403a00bf3c
Dzakus <Dzakus13@gmail.com>
2013-10-13 14:46:16 +0800

    BatteryBar Height Pref with Bar Preview + fixes
    1) Bar Preview Dialog in BatteryBar Height Preference
    2) Add String-Int Fixes for This + Previous Commit
    (Remove after a few versions.)
    3) Clean up code in SettingsActivity
    4) Partial SETTINGS_CHANGE broadcast implementation.
    
    (Original Commit Message by Dzakus : Live preview for Bar Height )

--- src/com/zst/xposed/xuimod/SettingActivity.java
+++ src/com/zst/xposed/xuimod/SettingActivity.java
@@ -8,38 +8,52 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 @SuppressLint("WorldReadableFiles")
 @SuppressWarnings("deprecation")
-public class SettingActivity extends PreferenceActivity implements OnPreferenceClickListener {
+public class SettingActivity extends PreferenceActivity implements
+		OnPreferenceClickListener {
+	public static final String TAG = SettingActivity.class.getSimpleName();
 	public static final String FIRST_KEY = "first";
-	public static final String PREFERENCE_FILE = Common.MY_PACKAGE_NAME + "_preferences";
+	public static final String PREFERENCE_FILE = Common.MY_PACKAGE_NAME
+			+ "_preferences";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		SharedPreferences prefs = this.getSharedPreferences(PREFERENCE_FILE, MODE_WORLD_READABLE);
+		SharedPreferences prefs = getSharedPreferences(PREFERENCE_FILE,
+				MODE_WORLD_READABLE);
 		// make a new preference so "MODE WORLD READABLE" works
-		if (!prefs.contains(FIRST_KEY)){
+		if (!prefs.contains(FIRST_KEY)) {
 			prefs.edit().putBoolean(FIRST_KEY, false).commit();
 		}
+
+		fixStringInt(prefs, Common.KEY_VOLUME_ALPHA,
+				Common.DEFAULT_VOLUME_ALPHA);
+		fixStringInt(prefs, Common.KEY_BATTERYBAR_HEIGHT,
+				Common.DEFAULT_BATTERYBAR_HEIGHT);
 
 		addPreferencesFromResource(R.xml.pref_setting);
 		findPreference("batterybar_restart").setOnPreferenceClickListener(this);
 		findPreference("seconds_restart").setOnPreferenceClickListener(this);
 		findPreference("listview_testing").setOnPreferenceClickListener(this);
+		Log.i(TAG, "onCreate");
 	}
 
 	@Override
 	public boolean onPreferenceClick(Preference p) {
-		if (p.getKey().equals("seconds_restart") || p.getKey().equals("batterybar_restart")) {
+		if (p.getKey().equals("seconds_restart")
+				|| p.getKey().equals("batterybar_restart")) {
 			SecondsClockMod.thix = null;
 			SecondsClockMod.enabled = false;
 			SecondsClockMod.stopForever = false;
@@ -54,15 +68,19 @@ public class SettingActivity extends PreferenceActivity implements OnPreferenceC
 	}
 
 	private void dialog_killSystemUI() {
-		new AlertDialog.Builder(this).setMessage(R.string.systemui_restart_dialog)
-				.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						killPackage("com.android.systemui");
-					}
-				}).setNegativeButton("NO", null).show();
+		new AlertDialog.Builder(this)
+				.setMessage(R.string.systemui_restart_dialog)
+				.setPositiveButton("YES",
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								killPackage("com.android.systemui");
+							}
+						}).setNegativeButton("NO", null).show();
 	}
 
+	// TODO Don't run shell on UI thread
 	private void killPackage(String packageToKill) {
 		// code modified from :
 		// http://forum.xda-developers.com/showthread.php?t=2235956&page=6
@@ -98,4 +116,18 @@ public class SettingActivity extends PreferenceActivity implements OnPreferenceC
 		builder.setView(modeList);
 		builder.show();
 	}
+
+	private void fixStringInt(SharedPreferences prefs, String key,
+			int defaultValue) {
+		// workaround. Previosuly, Common.KEY_* is String, but
+		// String doesn't cast to int
+		try {
+			prefs.getInt(key, 0);
+		} catch (Exception e) {
+			String defValueString = String.valueOf(defaultValue);
+			int value = Integer.valueOf(prefs.getString(key, defValueString));
+			prefs.edit().putInt(key, value).commit();
+		}
+	}
+
 }
